6f182671338a0d298a2354302ac753d20c93cc12
Nontown_Notch <nontown_notch@163.com>
2024-01-10 14:59:03 +0800

    Scene Update

--- Scene.py
+++ Scene.py
@@ -5,11 +5,11 @@ from random import randint, random
 
 from enum import Enum
 from Settings import *
-#from NPCs import *
-#from PopUpBox import *
-#from Portal import *
-#from BgmPlayer import *
-#from Tile import *
+from NPCs import *
+from PopUpBox import *
+from Portal import *
+from BgmPlayer import *
+from Tile import *
 
 class Scene():
     def __init__(self, window):
@@ -64,15 +64,27 @@ class MainMenu():
         self.bg = pygame.image.load(GamePath.menu)
         self.bg = pygame.transform.scale(self.bg, (WindowSettings.width, WindowSettings.height))
         self.font = pygame.font.Font(None, 36) #主界面文字大小
-        self.text1 = self.font.render("Press 1 to enter ...scene", True, (0, 0, 0)) #主界面文字内容1
+        self.text1 = self.font.render("Press 1 to enter wild scene", True, (0, 0, 0)) #主界面文字内容1
         self.textRect1 = self.text1.get_rect(center=(WindowSettings.width // 2, WindowSettings.height - 500)) #主界面文字位置1
-        self.text2 = self.font.render("Press 2 to enter ...scene", True, (0, 0, 0)) #主界面文字内容2
-        self.textRect2 = self.text2.get_rect(center=(WindowSettings.width // 2, WindowSettings.height - 400)) #主界面文字位置2
+        self.text2 = self.font.render("Press 2 to enter castle scene", True, (0, 0, 0)) #主界面文字内容2
+        self.textRect2 = self.text2.get_rect(center=(WindowSettings.width // 2, WindowSettings.height - 450)) #主界面文字位置2
+        self.text3 = self.font.render("Press 3 to enter temple scene", True, (0, 0, 0)) #主界面文字内容3
+        self.textRect3 = self.text3.get_rect(center=(WindowSettings.width // 2, WindowSettings.height - 400)) #主界面文字位置3
+        self.text4 = self.font.render("Press 4 to enter hut scene", True, (0, 0, 0)) #主界面文字内容4
+        self.textRect4 = self.text4.get_rect(center=(WindowSettings.width // 2, WindowSettings.height - 350)) #主界面文字位置4
+        self.text5 = self.font.render("Press 5 to enter battle scene", True, (0, 0, 0)) #主界面文字内容5
+        self.textRect5 = self.text5.get_rect(center=(WindowSettings.width // 2, WindowSettings.height - 300)) #主界面文字位置5
+        self.text6 = self.font.render("Press 6 to enter boss scene", True, (0, 0, 0)) #主界面文字内容6
+        self.textRect6 = self.text6.get_rect(center=(WindowSettings.width // 2, WindowSettings.height - 250)) #主界面文字位置6
 
     def render(self):
         self.window.blit(self.bg, (0, 0))
         self.window.blit(self.text1, self.textRect1) #显示主界面文字1
         self.window.blit(self.text2, self.textRect2) #显示主界面文字2
+        self.window.blit(self.text3, self.textRect3) #显示主界面文字3
+        self.window.blit(self.text4, self.textRect4) #显示主界面文字4
+        self.window.blit(self.text5, self.textRect5) #显示主界面文字5
+        self.window.blit(self.text6, self.textRect6) #显示主界面文字6
 
 class CityScene(Scene):
     def __init__(self, window):
